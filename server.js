09f9598ff2b5773bd7da5502602c890992a2876c
Matt Sichterman <msichterman1@gmail.com>
2020-11-10 21:43:43 -0600

    Added a basis for the node js api

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require("express");
+const path = require("path");
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/ping', (req, res) => {
-  return res.send('pong')
-})
+app.get("/ping", (req, res) => {
+  return res.send("pong");
+});
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 
-app.listen(8080)
+app.listen(8080);
