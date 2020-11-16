31a69cc35cd56289142eb05e28df070a0a32b858
Matt Sichterman <msichterman1@gmail.com>
2020-11-16 15:12:31 -0600

    Updated cors

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const port = 8080;
 
 app.use(express.static(path.join(__dirname, "build")));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Origin",
-    "https://main.dy3ohvv7548ac.amplifyapp.com/"
+    "https://main.dy3ohvv7548ac.amplifyapp.com"
   );
   res.header(
     "Access-Control-Allow-Headers",
